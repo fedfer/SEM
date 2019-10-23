d85e0354e45c5f135cd268ef36ec98a9f00ad21d
Federico Ferrari <ff31@duke.edu>
2019-10-23 13:00:02 -0400

    update plots

--- plot_data.R
+++ plot_data.R
@@ -2,18 +2,50 @@
 library(tidyverse)
 library(plyr)
 library(reshape2)
-load("~/SEM/nhanes_1516.RData")
 
 
 # matrix 0-1 for missing data
+load("~/SEM/data/nhanes_1516.RData")
 df_01 = df %>% is.na()
 df_01 %>% as.matrix() %>% image()
 
+# matrix 0-1 for Limit of Detection
+load("~/SEM/data/nhanes_chem_1516.RData")
+df_chem_noSEQN = df_chem %>% dplyr::select(-SEQN)
+lod =  df_chem_noSEQN %>% apply(., 2, function(x) min(x, na.rm = T))
+chem_lod = df_chem_noSEQN %>% apply(., 2, function(x) min(x, na.rm = T)) %>%
+  rep(., times = nrow(df_chem_noSEQN)) %>%
+  matrix(., nrow = nrow(df_chem_noSEQN), byrow = T)
 
-# Correlation matrix
-# Correlation plot
-Cor_plot = cor(df %>% as.matrix(),use = "pairwise.complete.obs")
-colnames(Cor_plot) = rownames(Cor_plot) = colnames(df)
+df_lod = (df_chem_noSEQN == chem_lod) %>% as.matrix() 
+df_lod %>% as.matrix() %>% image()
+lod_sum = apply(df_lod, 2 , function(x)  sum(x,na.rm = T))
+hist(lod_sum)
+
+# Correlation plot outcomes
+load("~/SEM/data/nhanes_out_1516.RData")
+Cor_plot = cor(df_out %>% as.matrix(),use = "pairwise.complete.obs")
+colnames(Cor_plot) = rownames(Cor_plot) = colnames(df_out)
+Cor_plot = melt(Cor_plot)
+ggplot(Cor_plot, aes(x = Var2, y = Var1)) + 
+  geom_tile(aes(fill=value), colour="grey20") + 
+  scale_fill_gradient2(low = "#191970", high = "#006400", mid = "white") +
+  theme(axis.title.x = element_blank(),
+        axis.title.y = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border = element_blank(),
+        panel.background = element_blank(),
+        axis.ticks = element_blank(),
+        axis.text = element_blank(),
+        axis.text.x = element_text(angle = 90, hjust = 1),
+        legend.title = element_text(),
+        plot.title = element_text(hjust = 0.5)) + 
+  labs(fill = " ") + 
+  ggtitle("Correlation Outcomes")
+
+# Correlation plot chemicals
+Cor_plot = cor(df_chem %>% as.matrix(),use = "pairwise.complete.obs")
+colnames(Cor_plot) = rownames(Cor_plot) = colnames(df_chem)
 Cor_plot = melt(Cor_plot)
 ggplot(Cor_plot, aes(x = Var2, y = Var1)) + 
   geom_tile(aes(fill=value), colour="grey20") + 
@@ -29,22 +61,16 @@ ggplot(Cor_plot, aes(x = Var2, y = Var1)) +
         legend.title = element_text(),
         plot.title = element_text(hjust = 0.5)) + 
   labs(fill = " ") + 
-  ggtitle("Correlation matrix")
+  ggtitle("Correlation Chemicals")
 
 
 
 # Plot two matrices
 library(reshape2)
-SampleMean = cbind(melt(sample_mean), label)
-label = "Aligned Sample Mean"
-ProcessMean = Reduce("+", aligned)/length(aligned)
-rownames(ProcessMean) = colnames(X)
-ProcessMean = cbind(melt(ProcessMean), label)
-ggdf = rbind(SampleMean, ProcessMean)
-ggplot(ggdf, aes(x = Var2, y = Var1)) + 
-  facet_grid(cols = vars(label)) +
+df_01_melt = melt(df_01)
+ggplot(df_01_melt, aes(x = Var2, y = Var1)) + 
   geom_tile(aes(fill=value), colour="grey20") + 
-  scale_fill_gradient2(low = "#800000", high = "#006400", mid = "white") +
+  #scale_fill_gradient2(low = "#800000", high = "#006400", mid = "white") +
   theme(axis.title.x = element_blank(),
         axis.title.y = element_blank(),
         panel.grid.major = element_blank(),
