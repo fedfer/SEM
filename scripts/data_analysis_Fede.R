142c31e0c4dc2e10a0518cab4dfc674cc32cb68e
Federico Ferrari <ff31@duke.edu>
2020-07-26 10:20:12 -0500

    longer chain

--- scripts/data_analysis_Fede.R
+++ scripts/data_analysis_Fede.R
@@ -196,8 +196,8 @@ sum(vec > 0.9)
 
 
 # Gibbs ------------------------------------------
-nrun = 1000
-burn = 500
+nrun = 1500
+burn = 1000
 n_samples = nrun - burn
 gibbs_result <- gibbs(X = X_hollow, Y = Y_hollow,
                       X_NA = X_NA, Y_NA = Y_NA, X_LOD = X_LOD, LOD_X_vec = LOD_X_vec, Z = Z,
